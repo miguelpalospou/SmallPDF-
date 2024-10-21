4ad40d884cc99f8b354d17fd9de3147a5cb60b34
Miguel Palos Pou <miguelpalospou@MacBook-Air-de-Miguel.local>
2024-10-21 11:13:15 +0200

    m

--- streamlit.py
+++ streamlit.py
@@ -17,11 +17,7 @@ st.markdown("<h1 style='color: #3498db; text-align: center;'>SmallPDF: Chat with
 st.subheader("API Keys")
 st.write("Make sure the Pinecone API and the OpenAI are included in the keys section")
 
-# Step 1: Input YouTube Link
-with st.sidebar:
-        st.markdown("<h3 style='color: #2ecc71;'>Menu:</h3>", unsafe_allow_html=True)
 
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
 
 # Step 3: Input the Pinecone Index Name
 index_name = st.text_input("Enter a name for the Pinecone Index:")
@@ -29,14 +25,19 @@ index_name = st.text_input("Enter a name for the Pinecone Index:")
 # Step 3: Input the Pinecone Index Name
 st.session_state.MODEL = st.text_input("Please enter the model you want to use in quotes (gpt-4o-mini or mistral:7b)")
 
+# Step 1: Input YouTube Link
+with st.sidebar:
+        st.markdown("<h3 style='color: #2ecc71;'>Menu:</h3>", unsafe_allow_html=True)
 
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
 
 # Ensure all necessary inputs are provided
-if st.button("Submit & Process"):
+        if st.button("Submit & Process"):
             with st.spinner("Processing..."):
                 text = getpdf.getpdf(pdf_docs)
                 text_chunks = getpdf.get_text_chunks(text)
                 st.session_state.vector = vectorstore.vectorstore(index_name=index_name, all_chunks=text_chunks, MODEL=st.session_state.MODEL)
+                st.success("Done!")
                 
 
 # Step 6: Ask Questions
